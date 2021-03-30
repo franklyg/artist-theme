d75304874dfad48d6bfb61fe59781f211426a63f
franklyg <f.gisonni@gmail.com>
2021-03-30 17:32:16 -0400

    response test

--- partials/navigation.js
+++ partials/navigation.js
@@ -13,24 +13,26 @@ function Navigation (){
   useEffect(() => {
 
     const fetchData = async () => {
-        var dataJSON = [],
-        api = []
-
         const SPOTIFY_ID = '0CXrZUvQjJYUiI0oztItS5';
-        const TRACK_TYPE = 'track'
+        const TRACK_TYPE = 'track';
+        fetch(`https://api.song.link/v1-alpha.1/links?url=spotify%3A${TRACK_TYPE}%3A${SPOTIFY_ID}&userCountry=US&key=9ab8abaf-c5f1-4edb-8e7f-7f72c7033693`)
+            .then(response => response.json())
+            .then(data => console.log(data));
+        // var dataJSON = [];
       
-        const res = await fetch(
-        `https://api.song.link/v1-alpha.1/links?url=spotify%3A${TRACK_TYPE}%3A${SPOTIFY_ID}&userCountry=US&key=9ab8abaf-c5f1-4edb-8e7f-7f72c7033693`, { 
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin" : "*", 
-                "Access-Control-Allow-Credentials" : true 
-            },
-            mode: 'no-cors' 
-        }
-        );
-        dataJSON = await res.json()
-        return setData(dataJSON)
+        // const res = await fetch(
+        
+        // { 
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "Access-Control-Allow-Origin" : "*", 
+        //         "Access-Control-Allow-Credentials" : true 
+        //     },
+        //     mode: 'no-cors' 
+        // }
+        // );
+        // dataJSON = await res.json();
+        // return setData(dataJSON)
     }
     fetchData()
   }, []);
