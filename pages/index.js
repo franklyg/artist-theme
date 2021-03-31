e6ebe0e8ec2aaa721f55c22f69de82869ee94175
franklyg <f.gisonni@gmail.com>
2021-03-30 23:11:54 -0400

    Pray for the API

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React, {useEffect, useState} from 'react';
 import Head from 'next/head'
 import { Client } from "../prismicKits";
 import SliceZone from "next-slicezone";
@@ -6,26 +5,8 @@ import { useGetStaticProps } from "next-slicezone/hooks";
 import Navigation from '../partials/navigation'
 import resolver from "../sm-resolver.js";
 
-const Page = (props) => {
-
-  const [data, setData] = useState();
-
-  useEffect(() => {
-
-    const fetchData = async () => {
-        var dataJSON = [];
-
-        const SPOTIFY_ID = '0CXrZUvQjJYUiI0oztItS5';
-        const TRACK_TYPE = 'track';
-      
-        const res = await fetch(`https://api.song.link/v1-alpha.1/links?url=spotify%3A${TRACK_TYPE}%3A${SPOTIFY_ID}&userCountry=US&key=9ab8abaf-c5f1-4edb-8e7f-7f72c7033693`);
-        dataJSON = await res.json();
-        console.log(dataJSON)
-        return setData(dataJSON)
-    }
-    fetchData()
-  }, []);
-
+const Page = ({ dataJSON }, props) => {
+  console.log(dataJSON)
   return (
     <>
       <Head>
@@ -38,11 +19,28 @@ const Page = (props) => {
 };
 
 // Fetch content from prismic
-export const getStaticProps = useGetStaticProps(
+export const getPrismicProps = useGetStaticProps(
   {
     client: Client(),
-    uid: () => "home"
+    uid: () => "home",
   }
 );
+
+export async function getStaticProps(context) {
+  
+  var dataJSON = [];
+
+  const SPOTIFY_ID = '0CXrZUvQjJYUiI0oztItS5';
+  const TRACK_TYPE = 'track';
+
+  const res = await fetch(`https://api.song.link/v1-alpha.1/links?url=spotify%3A${TRACK_TYPE}%3A${SPOTIFY_ID}&userCountry=US&key=9ab8abaf-c5f1-4edb-8e7f-7f72c7033693`);
+  dataJSON = await res.json();
+  // return setData(dataJSON)
+
+  return {
+    props: { dataJSON }
+  }
+
+}
 
 export default Page;
